c451a9ab678b86e2d9586c6edabfdac26234224b
Llewe <Llewe@users.noreply.github.com>
2025-09-08 15:37:29 +0200

    feat(BenchmarkUc): add benchmark use case protocols and update dependencies
    Introduced new protocols for benchmark use cases, covering operations such as creation, deletion, loading, and associations with model sets, metrics, plots, and solve jobs. Updated `dnspython` and `pytest-cov` dependencies to the latest versions.

--- luna_bench/_internal/usecases/benchmark/protocols.py
+++ luna_bench/_internal/usecases/benchmark/protocols.py
@@ -1,3 +1,5 @@
+
+
 from typing import Protocol
 
 from returns.result import Result
@@ -9,76 +11,43 @@ from luna_bench.errors.unknown_error import UnknownLunaBenchError
 
 
 class BenchmarkCreateUc(Protocol):
-    def __call__(self, benchmark_name: str) -> Result[BenchmarkDomain, DataNotUniqueError | UnknownLunaBenchError]: ...
-
+    def __call__(self, benchmark_name:str) -> Result[BenchmarkDomain, DataNotUniqueError| UnknownLunaBenchError]:...
 
 class BenchmarkDeleteUc(Protocol):
-    def __call__(self, benchmark_name: str) -> Result[None, DataNotExistError | UnknownLunaBenchError]: ...
-
+    def __call__(self, benchmark_name:str) -> Result[None, DataNotExistError| UnknownLunaBenchError]:...
 
 class BenchmarkLoadUc(Protocol):
-    def __call__(self, benchmark_name: str) -> Result[BenchmarkDomain, DataNotExistError | UnknownLunaBenchError]: ...
-
+    def __call__(self, benchmark_name:str) -> Result[BenchmarkDomain, DataNotExistError| UnknownLunaBenchError]:...
 
 class BenchmarkLoadAllUc(Protocol):
-    def __call__(self) -> Result[list[BenchmarkDomain], UnknownLunaBenchError]: ...
-
+    def __call__(self) -> Result[list[BenchmarkDomain], UnknownLunaBenchError]:...
 
 class BenchmarkSetModelsetUc(Protocol):
-    def __call__(
-        self, benchmark_name: str, modelset_name: str
-    ) -> Result[BenchmarkDomain, DataNotExistError | UnknownLunaBenchError]: ...
-
+    def __call__(self, benchmark_name:str, modelset_name:str) -> Result[BenchmarkDomain, DataNotExistError| UnknownLunaBenchError]:...
 
 class BenchmarkRemoveModelsetUc(Protocol):
-    def __call__(
-        self, benchmark_name: str, modelset_name: str
-    ) -> Result[BenchmarkDomain, DataNotExistError | UnknownLunaBenchError]: ...
-
+    def __call__(self, benchmark_name:str, modelset_name:str) -> Result[BenchmarkDomain, DataNotExistError| UnknownLunaBenchError]:...
 
 class BenchmarkAddModelMetricUc(Protocol):
-    def __call__(
-        self, benchmark_name: str, model_metric_name: str
-    ) -> Result[BenchmarkDomain, DataNotExistError | UnknownLunaBenchError]: ...
-
+    def __call__(self, benchmark_name:str, model_metric_name:str) -> Result[BenchmarkDomain, DataNotExistError| UnknownLunaBenchError]:...
 
 class BenchmarkRemoveModelMetricUc(Protocol):
-    def __call__(
-        self, benchmark_name: str, model_metric_name: str
-    ) -> Result[BenchmarkDomain, DataNotExistError | UnknownLunaBenchError]: ...
-
+    def __call__(self, benchmark_name:str, model_metric_name:str) -> Result[BenchmarkDomain, DataNotExistError| UnknownLunaBenchError]:...
 
 class BenchmarkAddMetricUc(Protocol):
-    def __call__(
-        self, benchmark_name: str, metric_name: str
-    ) -> Result[BenchmarkDomain, DataNotExistError | UnknownLunaBenchError]: ...
-
+    def __call__(self, benchmark_name:str, metric_name:str) -> Result[BenchmarkDomain, DataNotExistError| UnknownLunaBenchError]:...
 
 class BenchmarkRemoveMetricUc(Protocol):
-    def __call__(
-        self, benchmark_name: str, metric_name: str
-    ) -> Result[BenchmarkDomain, DataNotExistError | UnknownLunaBenchError]: ...
-
+    def __call__(self, benchmark_name:str, metric_name:str) -> Result[BenchmarkDomain, DataNotExistError| UnknownLunaBenchError]:...
 
 class BenchmarkAddPlotUc(Protocol):
-    def __call__(
-        self, benchmark_name: str, plot_name: str
-    ) -> Result[BenchmarkDomain, DataNotExistError | UnknownLunaBenchError]: ...
-
+    def __call__(self, benchmark_name:str, plot_name:str) -> Result[BenchmarkDomain, DataNotExistError| UnknownLunaBenchError]:...
 
 class BenchmarkRemovePlotUc(Protocol):
-    def __call__(
-        self, benchmark_name: str, plot_name: str
-    ) -> Result[BenchmarkDomain, DataNotExistError | UnknownLunaBenchError]: ...
-
+    def __call__(self, benchmark_name:str, plot_name:str) -> Result[BenchmarkDomain, DataNotExistError| UnknownLunaBenchError]:...
 
 class BenchmarkAddSolveJobUc(Protocol):
-    def __call__(
-        self, benchmark_name: str, solvejob_name: str
-    ) -> Result[BenchmarkDomain, DataNotExistError | UnknownLunaBenchError]: ...
-
+    def __call__(self, benchmark_name:str, solvejob_name:str) -> Result[BenchmarkDomain, DataNotExistError| UnknownLunaBenchError]:...
 
 class BenchmarkRemoveSolveJobUc(Protocol):
-    def __call__(
-        self, benchmark_name: str, solvejob_name: str
-    ) -> Result[BenchmarkDomain, DataNotExistError | UnknownLunaBenchError]: ...
+    def __call__(self, benchmark_name:str, solvejob_name:str) -> Result[BenchmarkDomain, DataNotExistError| UnknownLunaBenchError]:...
