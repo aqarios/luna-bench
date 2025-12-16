b6aaa091657a2585d5136e9910e0c10593d2ccc4
Maximilian Janetschek <61047435+MaximilianJanetschek@users.noreply.github.com>
2025-12-16 11:03:05 +0100

    Added local scip algorithm. (#156)
    * Added local scip algorithm.
    
    * Added test cases for scip.py
    
    * Refactoring.
    
    * Added the ability to set scip logging level.
    
    * Refactoring and docstrings update.

--- luna_bench/components/algorithms/__init__.py
+++ luna_bench/components/algorithms/__init__.py
@@ -1,4 +1,9 @@
 from .failing_algorithm import FailingAlgorithm
 from .fake_algorithm import FakeAlgorithm
+from .scip import ScipAlgorithm
 
-__all__ = ["FailingAlgorithm", "FakeAlgorithm"]
+__all__ = [
+    "FailingAlgorithm",
+    "FakeAlgorithm",
+    "ScipAlgorithm"
+]
