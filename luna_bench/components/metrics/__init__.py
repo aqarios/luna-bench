688c90da7a2136ef108a71e3da6bc688e56d326d
MaximilianJanetschek <maximilian.janetschek@t-online.de>
2026-02-09 11:51:32 +0100

    ignore

--- luna_bench/components/metrics/__init__.py
+++ luna_bench/components/metrics/__init__.py
@@ -1,6 +1,10 @@
 from .best_solution_found import BestSolutionFound, BestSolutionFoundResult
 from .fake_metric import FakeMetric
 from .feasbility_ratio import FeasibilityRatio, FeasibilityRatioResult
+from .fraction_of_overall_best_solution import (
+    FractionOfOverallBestSolution,
+    FractionOfOverallBestSolutionResult,
+)
 from .runtime import Runtime, RuntimeResult
 from .time_to_solution import TimeToSolution, TimeToSolutionResult
 
@@ -10,6 +14,8 @@ __all__ = [
     "FakeMetric",
     "FeasibilityRatio",
     "FeasibilityRatioResult",
+    "FractionOfOverallBestSolution",
+    "FractionOfOverallBestSolutionResult",
     "Runtime",
     "RuntimeResult",
     "TimeToSolution",
