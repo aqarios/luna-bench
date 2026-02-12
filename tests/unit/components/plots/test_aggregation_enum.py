182e42055d4629553510a4da09be359c40330444
Maximilian Janetschek <maximilian.janetschek@t-online.de>
2026-02-12 23:01:58 +0100

    leveraged parametrized test for aggregation enum errorbar.

--- tests/unit/components/plots/test_aggregation_enum.py
+++ tests/unit/components/plots/test_aggregation_enum.py
@@ -1,27 +1,18 @@
+from playhouse.sqlite_udf import aggregate
+
 from luna_bench.components.plots.utils.aggregation_enum import Aggregation
+import pytest
 
 
 class TestAggregation:
-    def test_mean_sd_estimator(self) -> None:
-        assert Aggregation.MEAN_SD.estimator == "mean"
-
-    def test_mean_sd_errorbar(self) -> None:
-        assert Aggregation.MEAN_SD.errorbar == "sd"
-
-    def test_mean_estimator(self) -> None:
-        assert Aggregation.MEAN.estimator == "mean"
-
-    def test_mean_errorbar(self) -> None:
-        assert Aggregation.MEAN.errorbar is None
-
-    def test_max_estimator(self) -> None:
-        assert Aggregation.MAX.estimator == "max"
-
-    def test_max_errorbar(self) -> None:
-        assert Aggregation.MAX.errorbar is None
-
-    def test_min_estimator(self) -> None:
-        assert Aggregation.MIN.estimator == "min"
-
-    def test_min_errorbar(self) -> None:
-        assert Aggregation.MIN.errorbar is None
+    @pytest.mark.parametrize(
+        ("aggregation", "estimator", "errorbar"),[
+            (Aggregation.MEAN_SD, "mean", "sd"),
+            (Aggregation.MEAN, "mean", None),
+            (Aggregation.MAX, "max", None),
+            (Aggregation.MIN, "min", None),
+        ],
+    )
+    def test_estimator_errorbar(self, aggregation: Aggregation, estimator: str | None, errorbar: str | None) -> None:
+        assert aggregation.estimator == estimator
+        assert aggregation.errorbar == errorbar
