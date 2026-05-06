6db20b8fc6bc186be97e0aec9d986ccf6b818a14
Llewe <16879918+Llewe@users.noreply.github.com>
2026-05-06 12:00:56 +0200

    - added pytest-env to set env var before huey is configured

--- tests/__init__.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.environ["DB_CONNECTION_STRING"] = ":memory:"  # Otherwise we create a db file each time we run tests
-os.environ["DB_JOBS_CONNECTION_STRING"] = ":memory:"  # Otherwise we create a db file each time we run tests
